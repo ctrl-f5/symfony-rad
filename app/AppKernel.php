36746cb3f9296b519e0fe1fdeb03ca704290cad8
Nicky De Maeyer <nicky.demaeyer@gmail.com>
2015-05-15 16:28:09 +0200

    added knpmenu

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new Trsteel\CkeditorBundle\TrsteelCkeditorBundle(),
             new Ctrl\RadBundle\CtrlRadBundle(),
             new AppBundle\AppBundle(),
